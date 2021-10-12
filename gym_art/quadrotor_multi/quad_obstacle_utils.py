6940ab2cd4d0601674ce3fe28e0443263bf446a5
Zhehui-Huang <zhehuihu@usc.edu>
2021-10-12 00:12:19 -0700

    Change the pos of obstacles in the obstacle mode

--- gym_art/quadrotor_multi/quad_obstacle_utils.py
+++ gym_art/quadrotor_multi/quad_obstacle_utils.py
@@ -4,5 +4,5 @@ OBSTACLES_SHAPE_LIST = ['sphere', 'cube']
 Y_VALUE = 0.0
 STATIC_OBSTACLE_DOOR = np.array([
                                 [-1.0, Y_VALUE, 2.5], [0.0, Y_VALUE, 2.5], [1.0, Y_VALUE, 2.5],
-                                [-0.65, Y_VALUE, 1.5],                      [0.65, Y_VALUE, 1.5],
+                                [-1.0, Y_VALUE, 1.5],                      [1.0, Y_VALUE, 1.5],
                                 [-1.0, Y_VALUE, 0.5], [0.0, Y_VALUE, 0.5], [1.0, Y_VALUE, 0.5]])
