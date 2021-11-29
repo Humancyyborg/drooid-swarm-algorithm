8bd0f5a4a49208b9a8f786a3568bdebb36fbc0cc
Zhehui-Huang <zhehuihu@usc.edu>
2021-11-29 14:40:12 -0800

    Add runner scripts for obstacles

---
+++ swarm_rl/runs/quad_multi_deepsets_through_pillar_test_rew_scale_v8.py
@@ -0,0 +1,35 @@
+from sample_factory.runner.run_description import RunDescription, Experiment, ParamGrid
+
+from swarm_rl.runs.quad_multi_deepsets_obstacle_baseline import QUAD_OBSTACLE_PILLAR_BASELINE_CLI
+
+# quads_obstacle_num: 16
+_params = ParamGrid([
+    ('seed', [0000, 1111, 2222, 3333]),
+    ('hidden_size', [256]),
+    ('quads_neighbor_hidden_size', [128]),
+    ('quads_obstacle_hidden_size', [128]),
+    ('reward_scale', [0.2]),
+    ('quads_obst_penalty_fall_off', [10.0]),
+])
+
+_experiment = Experiment(
+    'v8_test_scale_through_pillar',
+    QUAD_OBSTACLE_PILLAR_BASELINE_CLI,
+    _params.generate_params(randomize=False),
+)
+
+RUN_DESCRIPTION = RunDescription('v8_test_scale_quads_multi_obst_through_pillar_8a_v116', experiments=[_experiment])
+
+# On Brain server, when you use num_workers = 72, if the system reports: Resource temporarily unavailable,
+# then, try to use two commands below
+# export OMP_NUM_THREADS=1
+# export USE_SIMPLE_THREADED_LEVEL3=1
+
+# Command to use this script on server:
+# xvfb-run python -m runner.run --run=quad_multi_through_hole_obstacle --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
+# Command to use this script on local machine:
+# Please change num_workers to the physical cores of your local machine
+# python -m runner.run --run=quad_multi_through_hole_obstacle --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
+
+# Slurm
+# srun --exclusive -c72 -N1 --gres=gpu:4 python -m sample_factory.runner.run --run=swarm_rl.runs.quad_multi_deepsets_through_random_obstacles --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
