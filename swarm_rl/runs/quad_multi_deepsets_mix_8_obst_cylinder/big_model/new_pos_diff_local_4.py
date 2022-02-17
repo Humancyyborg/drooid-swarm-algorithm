6104bb94f650da68392b8b156234abf1abed6956
Zhehui-Huang <zhehuihu@usc.edu>
2022-02-16 22:20:13 -0800

    Add a runner script for 4 local obst

---
+++ swarm_rl/runs/quad_multi_deepsets_mix_8_obst_cylinder/big_model/new_pos_diff_local_4.py
@@ -0,0 +1,37 @@
+from sample_factory.runner.run_description import RunDescription, Experiment, ParamGrid
+
+from swarm_rl.runs.quad_multi_deepsets_obstacle_baseline import QUAD_8_OBSTACLES_PARAMETERZE_CLI, seeds
+
+_params = ParamGrid([
+    ('seed', seeds(2)),
+    ('quads_local_obst_obs', [4]),
+    ('num_workers', [72]),
+])
+
+BIG_MODEL_CLI = QUAD_8_OBSTACLES_PARAMETERZE_CLI + (
+    ' --hidden_size=256 --quads_neighbor_hidden_size=128 --quads_obstacle_hidden_size=128 --quads_obst_level_mode=0 '
+    '--with_wandb=False --quads_use_pos_diff=True --quads_obstacle_type=cylinder --quads_pos_diff_reward=500 '
+    '--quads_pos_diff_decay_rate=0.999'
+)
+
+_experiment = Experiment(
+    'local_4-new_pos_diff-cylinder-big_model',
+    BIG_MODEL_CLI,
+    _params.generate_params(randomize=False),
+)
+
+RUN_DESCRIPTION = RunDescription('8_obst_2_local_quads_multi_obst_mix_8a_v116', experiments=[_experiment])
+
+# On Brain server, when you use num_workers = 72, if the system reports: Resource temporarily unavailable,
+# then, try to use two commands below
+# export OMP_NUM_THREADS=1
+# export USE_SIMPLE_THREADED_LEVEL3=1
+
+# Command to use this script on server:
+# xvfb-run python -m runner.run --run=quad_multi_through_hole_obstacle --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
+# Command to use this script on local machine:
+# Please change num_workers to the physical cores of your local machine
+# python -m runner.run --run=quad_multi_through_hole_obstacle --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
+
+# Slurm
+# srun --exclusive -c72 -N1 --gres=gpu:4 python -m sample_factory.runner.run --run=swarm_rl.runs.quad_multi_deepsets_through_random_obstacles --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
