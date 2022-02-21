3a99395316a2663c590dfbc6ec15cbfac25f0597
Zhehui-Huang <zhehuihu@usc.edu>
2022-02-21 04:25:14 -0800

    Add a runner script to compare with GLAS

---
+++ swarm_rl/runs/quad_multi_deepsets_mix_8_obst_cylinder/big_model/quad_multi_mix_obst_compare_glas.py
@@ -0,0 +1,41 @@
+from sample_factory.runner.run_description import RunDescription, Experiment, ParamGrid
+
+from swarm_rl.runs.quad_multi_deepsets_obstacle_baseline import QUAD_8_OBSTACLES_PARAMETERZE_CLI, seeds
+
+_params = ParamGrid([
+    ('seed', [1161130, 9171076, 3137463, 3386884]),
+    ('hidden_size', [256]),
+    ('quads_neighbor_hidden_size', [128]),
+    ('quads_obstacle_hidden_size', [128]),
+    ('quads_obstacle_num', [6]),
+    ('quads_obstacle_size', [1.0]),
+])
+
+SMALL_MODEL_CLI = QUAD_8_OBSTACLES_PARAMETERZE_CLI + (
+    ' --num_workers=32 --quads_obstacle_type=cylinder --quads_local_obst_obs=2 '
+    '--quads_obst_level_mode=1 --with_wandb=False --quads_obstacle_type=cylinder '
+    '--quads_apply_downwash=False --quads_obstacle_num=6 '
+    '--quads_use_pos_diff=True --quads_episode_duration=20.0'
+)
+
+_experiment = Experiment(
+    'glas-curri-256-hidden-cylinder-big_model',
+    SMALL_MODEL_CLI,
+    _params.generate_params(randomize=False),
+)
+
+RUN_DESCRIPTION = RunDescription('curri-6_obst_quads_multi_obst_mix_8a_v116', experiments=[_experiment])
+
+# On Brain server, when you use num_workers = 72, if the system reports: Resource temporarily unavailable,
+# then, try to use two commands below
+# export OMP_NUM_THREADS=1
+# export USE_SIMPLE_THREADED_LEVEL3=1
+
+# Command to use this script on server:
+# xvfb-run python -m runner.run --run=quad_multi_through_hole_obstacle --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
+# Command to use this script on local machine:
+# Please change num_workers to the physical cores of your local machine
+# python -m runner.run --run=quad_multi_through_hole_obstacle --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
+
+# Slurm
+# srun --exclusive -c72 -N1 --gres=gpu:4 python -m sample_factory.runner.run --run=swarm_rl.runs.quad_multi_deepsets_through_random_obstacles --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
