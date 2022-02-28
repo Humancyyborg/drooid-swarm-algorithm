1a13e0a2578e4f568a2a7f037164b6d8e55281ae
Zhehui-Huang <zhehuihu@usc.edu>
2022-02-28 00:12:12 -0800

    Add runner scripts

---
+++ swarm_rl/runs/quad_multi_deepsets_mix_8_obst_cylinder/small_model/quad_multi_mix_obst_grid_search_grad_enable_obst_col.py
@@ -0,0 +1,40 @@
+from sample_factory.runner.run_description import RunDescription, Experiment, ParamGrid
+
+from swarm_rl.runs.quad_multi_deepsets_obstacle_baseline import QUAD_8_OBSTACLES_PARAMETERZE_CLI, seeds
+
+_params = ParamGrid([
+    ('seed', seeds(4)),
+])
+
+SMALL_MODEL_CLI = QUAD_8_OBSTACLES_PARAMETERZE_CLI + (
+    ' --num_workers=36 --hidden_size=16 --quads_neighbor_hidden_size=16 --quads_obstacle_hidden_size=16 '
+    '--quads_obstacle_type=cylinder --quads_obstacle_num=10 --quads_local_obst_obs=-1 --quads_obstacle_size=0.6 '
+    '--obst_obs_type=pos_vel_size --quads_obst_level_mode=1 --quads_obst_level=1000000 --quads_curriculum_min_obst=10 '
+    '--neighbor_obs_type=pos_vel_size --quads_local_obs=-1 '
+    '--nearest_nbrs=9 --quads_apply_downwash=True '
+    '--quads_use_pos_diff=False --quads_episode_duration=20.0 '
+    '--anneal_start_collision_steps=300000000 --anneal_collision_steps=300000000 '
+    '--with_wandb=False'
+)
+
+_experiment = Experiment(
+    'curri-16-hidden-cylinder-small_model',
+    SMALL_MODEL_CLI,
+    _params.generate_params(randomize=False),
+)
+
+RUN_DESCRIPTION = RunDescription('curri-6_obst_quads_multi_obst_mix_8a_v116', experiments=[_experiment])
+
+# On Brain server, when you use num_workers = 72, if the system reports: Resource temporarily unavailable,
+# then, try to use two commands below
+# export OMP_NUM_THREADS=1
+# export USE_SIMPLE_THREADED_LEVEL3=1
+
+# Command to use this script on server:
+# xvfb-run python -m runner.run --run=quad_multi_through_hole_obstacle --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
+# Command to use this script on local machine:
+# Please change num_workers to the physical cores of your local machine
+# python -m runner.run --run=quad_multi_through_hole_obstacle --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
+
+# Slurm
+# srun --exclusive -c72 -N1 --gres=gpu:4 python -m sample_factory.runner.run --run=swarm_rl.runs.quad_multi_deepsets_through_random_obstacles --runner=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=1 --num_gpus=4
