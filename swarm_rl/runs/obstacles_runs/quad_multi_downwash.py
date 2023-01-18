18e79f4dd4d6a78db20b6510497a6421ea10dbe5
rahul-krupani <rahul.krupani@outlook.com>
2023-01-17 16:23:37 -0800

    Fixed Runs files

--- swarm_rl/runs/obstacles_runs/quad_multi_downwash.py
+++ swarm_rl/runs/obstacles_runs/quad_multi_downwash.py
@@ -8,10 +8,9 @@ _params = ParamGrid(
 )
 
 OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
-    '--train_for_env_steps=50000000 --num_workers=4 --use_obstacles=True --quads_obstacle_num=8 '
-    '--quads_obst_collision_smooth_max_penalty=10.0 --quads_obst_collision_reward=5.0 '
-    #'--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_group=testing-new-runs --wandb_user=multi-drones '
-    '--anneal_collision_steps=300000000'#--room_dims 10 10 6'
+    '--train_for_env_steps=250000000 --num_workers=4 --use_obstacles=False --use_downwash=True '
+    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_group=downwash --wandb_user=multi-drones '
+    '--anneal_collision_steps=300000000'
 )
 
 _experiment = Experiment(
@@ -20,5 +19,5 @@ _experiment = Experiment(
     _params.generate_params(randomize=False),
 )
 
-RUN_DESCRIPTION = RunDescription("quad_multi_baseline", experiments=[_experiment])
+RUN_DESCRIPTION = RunDescription("quad_multi_downwash", experiments=[_experiment])
 # python -m sample_factory.launcher.run --run=swarm_rl.runs.sf2_multi_drone --runner=processes --max_parallel=1 --pause_between=1 --experiments_per_gpu=4 --num_gpus=1
