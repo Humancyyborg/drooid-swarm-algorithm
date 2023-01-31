e4a448b7468f7cb64bea0f94f74ed43856f5ba28
yang-zj1026 <victoryang1026@gmail.com>
2023-01-30 19:13:18 -0800

    bug fix

--- swarm_rl/runs/quad_multi_mix_baseline.py
+++ swarm_rl/runs/quad_multi_mix_baseline.py
@@ -36,7 +36,7 @@ QUAD_BASELINE_CLI_8 = (
     '--quads_collision_reward=5.0 --quads_collision_smooth_max_penalty=10.0 '
     '--quads_neighbor_encoder_type=attention --replay_buffer_sample_prob=0.75 --save_milestones_sec=1800 '
     '--normalize_input=False --normalize_returns=False --reward_clip=10 '
-    '----anneal_collision_steps=300000000'
+    '--anneal_collision_steps=300000000'
 )
 
 # For scale, need to change
