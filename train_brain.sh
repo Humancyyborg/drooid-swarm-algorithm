e2eb06cd7ebe45f6a1c8f956f3abd3d991386bc8
yang-zj1026 <victoryang1026@gmail.com>
2023-02-02 15:57:29 -0800

    update training script on brain server

--- train_brain.sh
+++ train_brain.sh
@@ -3,5 +3,5 @@ python -m sample_factory.launcher.run \
 --backend=slurm --slurm_workdir=slurm_output \
 --experiment_suffix=slurm --pause_between=1 \
 --slurm_gpus_per_job=1 --slurm_cpus_per_gpu=16 \
---slurm_sbatch_template=/home/rkrupani/slurm/sbatch_template.sh \
+--slurm_sbatch_template=/home/zhaojing/sbatch.sh \
 --slurm_print_only=False
