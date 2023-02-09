e9b02b865171d0e7eaa80459e74962fab3c65e38
yang-zj1026 <victoryang1026@gmail.com>
2023-02-08 17:14:36 -0800

    bug fix

--- swarm_rl/runs/obstacles_runs/quad_single_obstacles.py
+++ swarm_rl/runs/obstacles_runs/quad_single_obstacles.py
@@ -8,7 +8,7 @@ _params = ParamGrid(
 )
 
 OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
-    '--train_for_env_steps=1000000000 --num_workers=24 ----num_envs_per_worker=8 '
+    '--train_for_env_steps=1000000000 --num_workers=24 --num_envs_per_worker=8 '
     '--use_obstacles=True --quads_obstacle_num=8 --quads_obstacle_size=0.3 --quads_collision_obst_falloff_radius=3.0 '
     '--quads_obst_collision_smooth_max_penalty=2.0 --quads_obst_collision_reward=5.0 '
     '--quads_neighbor_hidden_size=256 --neighbor_obs_type=none --quads_neighbor_encoder_type=no_encoder '
