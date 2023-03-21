90dfea226cc34b82b126e9ee7ebfe8a932818d16
Zhehui Huang <zhehuihu@usc.edu>
2023-03-21 14:16:09 -0700

    Test more: Restart refactor v1

--- swarm_rl/runs/sf2_quads_8_test.py
+++ swarm_rl/runs/sf2_quads_8_test.py
@@ -7,11 +7,11 @@ _params = ParamGrid([
 
 MULTI_8_CLI = QUAD_BASELINE_CLI_8 + (
     ' --quads_neighbor_encoder_type=attention --with_wandb=True --wandb_project=Quad-Swarm-RL '
-    '--wandb_group=restart_refactor_v1 --wandb_user=multi-drones'
+    '--wandb_group=sf2_restart_refactor_v1 --wandb_user=multi-drones'
 )
 
 _experiment = Experiment(
-    'restart_refactor_v1',
+    'sf2_restart_refactor_v1',
     MULTI_8_CLI,
     _params.generate_params(randomize=False),
 )
