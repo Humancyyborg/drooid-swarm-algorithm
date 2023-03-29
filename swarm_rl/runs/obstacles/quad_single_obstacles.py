a60430466f420bee41a2d5f0edbb1b1082bfac97
Zhehui Huang <zhehuihu@usc.edu>
2023-03-29 10:49:09 -0700

    Restart refactor v1.13: Modify the runner script

--- swarm_rl/runs/obstacles/quad_single_obstacles.py
+++ swarm_rl/runs/obstacles/quad_single_obstacles.py
@@ -10,11 +10,11 @@ _params = ParamGrid(
 OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     ' --num_workers=36 --num_envs_per_worker=8 --quads_num_agents=1 '
     '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones '
-    '--wandb_group=restart_refactor_obstacle_v1_2'
+    '--wandb_group=restart_refactor_obstacle_v1_3'
 )
 
 _experiment = Experiment(
-    "restart_refactor_obstacle_v1_2",
+    "restart_refactor_obstacle_v1_3",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
