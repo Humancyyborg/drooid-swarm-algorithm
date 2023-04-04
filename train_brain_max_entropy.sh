fbcc0b0efd8065c2740443060278ca0eaf1fb3a6
Zhehui Huang <zhehuihu@usc.edu>
2023-04-04 13:03:13 -0700

    modify bash scripts

--- train_brain_max_entropy.sh
+++ train_brain_max_entropy.sh
@@ -3,5 +3,5 @@ python -m sample_factory.launcher.run \
 --backend=slurm --slurm_workdir=slurm_output \
 --experiment_suffix=slurm --pause_between=1 \
 --slurm_gpus_per_job=1 --slurm_cpus_per_gpu=16 \
---slurm_sbatch_template=/home/zhehui/slurm/swarm_rl_sbatch_timeout.sh \
+--slurm_sbatch_template=/home/zhehui/slurm/restart-swarm-rl_sbatch_timeout.sh \
 --slurm_print_only=False
