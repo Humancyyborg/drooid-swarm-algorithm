ccf3ad400a8682c9e3159963212f0db6d7bc4ba6
Zhehui Huang <zhehuihu@usc.edu>
2023-04-08 15:32:08 -0700

    Modify running scripts

--- swarm_rl/runs/obstacles/quads_multi_obstacles_max_entro.py
+++ swarm_rl/runs/obstacles/quads_multi_obstacles_max_entro.py
@@ -10,7 +10,7 @@ _params = ParamGrid(
 
 OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     ' --num_workers=36 --num_envs_per_worker=4 --quads_num_agents=8 '
-    '--max_entropy_coeff=0.0001 --train_for_env_steps=10000000000 '
+    '--max_entropy_coeff=0.0005 --train_for_env_steps=100000000000 '
     '--quads_neighbor_visible_num=6 --quads_neighbor_obs_type=pos_vel --quads_encoder_type=attention '
     '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones '
     '--wandb_group=restart_refactor_obstacle_multi_attn_explor_loss_v1'
