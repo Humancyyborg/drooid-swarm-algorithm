04103b80f096ff7906494ba68791d53528d2820e
Zhehui Huang <zhehuihu@usc.edu>
2023-04-10 12:57:02 -0700

    Modify scripts for rollout length

--- swarm_rl/runs/obstacles/quads_multi_obstacles_rollout.py
+++ swarm_rl/runs/obstacles/quads_multi_obstacles_rollout.py
@@ -10,15 +10,15 @@ _params = ParamGrid(
 )
 
 OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
-    ' --num_workers=36 --num_envs_per_worker=4 --quads_num_agents=8 '
+    ' --num_workers=16 --num_envs_per_worker=2 --quads_num_agents=8 '
     '--max_entropy_coeff=0.0 --train_for_env_steps=100000000000 '
     '--quads_neighbor_visible_num=6 --quads_neighbor_obs_type=pos_vel --quads_encoder_type=attention '
     '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones '
-    '--wandb_group=restart_refactor_obstacle_multi_attn_rollout_v1'
+    '--wandb_group=restart_refactor_obstacle_multi_attn_rollout_v2'
 )
 
 _experiment = Experiment(
-    "restart_refactor_obstacle_multi_attn_rollout_v1",
+    "restart_refactor_obstacle_multi_attn_rollout_v2",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
