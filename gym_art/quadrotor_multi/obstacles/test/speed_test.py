3dcf471b49fde6fa2d6fcb568e48cb3195e9c66e
Zhehui Huang <zhehuihu@usc.edu>
2023-04-29 12:36:44 -0700

    Support one more obstacle representation: lidar scan

--- gym_art/quadrotor_multi/obstacles/test/speed_test.py
+++ gym_art/quadrotor_multi/obstacles/test/speed_test.py
@@ -16,10 +16,24 @@ def test_speed_get_cell_centers():
     print('get_cell_centers - Mean Time:   ', np.mean(times[1:]))
 
 
+def test_speed_get_surround_sdfs_radar_2d():
+    SETUP_CODE = '''from gym_art.quadrotor_multi.obstacles.utils import get_surround_sdfs_radar_2d;import numpy as np'''
+
+    TEST_CODE = '''quad_poses=np.array([[0., 0.], [0.5, 0.5], [-0.5, 0.5], [0.8, 0.5], [-0.5, 0.5], [-0.1, 0.9], [1.2, 0.8], [2.5, 1.5]]); obst_poses=np.array([[0., 0.95], [1.5, 2.5], [-0.5, 1.5], [-2.5, 0.5], [-3.5, 1.5], [0.0, 2.5], [1.5, 1.5], [2.5, -1.5]]); quad_vels=np.array([[0.11, 0.25], [-0.5, 0.11], [-1.5, -0.68], [0.77, 0.0], [0.0, 0.0], [0.0, 0.89], [0.25, 0.001], [2.5, -2.2]]);obst_radius=0.3;scan_range=1.5*np.pi;ray_num=8;get_surround_sdfs_radar_2d(quad_poses, obst_poses, quad_vels, obst_radius, scan_range, ray_num)'''
+
+    times = timeit.repeat(setup=SETUP_CODE,
+                          stmt=TEST_CODE,
+                          repeat=5,
+                          number=int(1e4))
+
+    print('get_surround_sdfs_radar_2d - Mean Time:   ', np.mean(times[1:]))
+
+
 def speed_test():
     test_speed_get_cell_centers()
+    test_speed_get_surround_sdfs_radar_2d()
     return
 
 
 if __name__ == "__main__":
-    test_speed_get_cell_centers()
+    speed_test()
