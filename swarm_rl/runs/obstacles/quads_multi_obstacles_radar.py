90b8d38ffa15dd651cc38d8c174fadc3519416c5
Zhehui Huang <zhehuihu@usc.edu>
2023-04-29 15:25:52 -0700

    Add running scripts

--- swarm_rl/runs/obstacles/quads_multi_obstacles_radar.py
+++ swarm_rl/runs/obstacles/quads_multi_obstacles_radar.py
@@ -4,8 +4,8 @@ from swarm_rl.runs.obstacles.quad_obstacle_baseline import QUAD_BASELINE_CLI_8
 _params = ParamGrid(
     [
         ("seed", [0000, 1111]),
-        ("quads_obstacle_scan_range", [120, 180]),
-        ("quads_obstacle_ray_num", [4, 8, 12]),
+        ("quads_obstacle_scan_range", [180]),
+        ("quads_obstacle_ray_num", [6, 12]),
     ]
 )
 
@@ -13,11 +13,11 @@ OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     ' --quads_obstacle_obs_type=radar --num_workers=36 --num_envs_per_worker=4 --quads_num_agents=8 '
     '--quads_neighbor_visible_num=6 --quads_neighbor_obs_type=pos_vel --quads_encoder_type=attention '
     '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones '
-    '--wandb_group=restart_refactor_obstacle_multi_attn'
+    '--wandb_group=restart_refactor_obstacle_multi_attn_radar'
 )
 
 _experiment = Experiment(
-    "restart_refactor_obstacle_multi",
+    "restart_refactor_obstacle_multi_radar",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
