c289cd75f218beacffefbb020df9a6c18e6677bb
Zhehui Huang <zhehuihu@usc.edu>
2023-06-01 16:52:32 -0700

    Modify runner script

--- swarm_rl/runs/obstacles/quads_multi_obstacles.py
+++ swarm_rl/runs/obstacles/quads_multi_obstacles.py
@@ -3,7 +3,8 @@ from swarm_rl.runs.obstacles.quad_obstacle_baseline import QUAD_BASELINE_CLI_8
 
 _params = ParamGrid(
     [
-        ("seed", [0000, 1111, 2222, 3333]),
+        ("seed", [0000, 1111]),
+        ("max_policy_lag", [150, 100000000]),
     ]
 )
 
@@ -11,11 +12,11 @@ OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     ' --num_workers=36 --num_envs_per_worker=4 --quads_num_agents=8 '
     '--quads_neighbor_visible_num=6 --quads_neighbor_obs_type=pos_vel --quads_encoder_type=attention '
     '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones '
-    '--wandb_group=restart_refactor_obstacle_multi_attn'
+    '--wandb_group=final_policy_lag'
 )
 
 _experiment = Experiment(
-    "restart_refactor_obstacle_multi",
+    "final_policy_lag",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
