6b1d9538c571dd3def7aca2a15e327f52d97d29f
Zhehui Huang <zhehuihu@usc.edu>
2023-06-01 17:49:35 -0700

    Add running script & change bash

---
+++ swarm_rl/runs/obstacles/quads_multi_obstacles_new_metric.py
@@ -0,0 +1,23 @@
+from sample_factory.launcher.run_description import Experiment, ParamGrid, RunDescription
+from swarm_rl.runs.obstacles.quad_obstacle_baseline import QUAD_BASELINE_CLI_8
+
+_params = ParamGrid(
+    [
+        ("seed", [0000, 1111]),
+    ]
+)
+
+OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
+    ' --num_workers=36 --num_envs_per_worker=4 --quads_num_agents=8 '
+    '--quads_neighbor_visible_num=6 --quads_neighbor_obs_type=pos_vel --quads_encoder_type=attention '
+    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones '
+    '--wandb_group=final_new_metric'
+)
+
+_experiment = Experiment(
+    "final_new_metric",
+    OBSTACLE_MODEL_CLI,
+    _params.generate_params(randomize=False),
+)
+
+RUN_DESCRIPTION = RunDescription("obstacles_multi", experiments=[_experiment])
