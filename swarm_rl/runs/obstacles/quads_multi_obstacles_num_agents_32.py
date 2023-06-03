d1046eef0741474eeaea44b2e1fbb377eb1ecd0a
Zhehui Huang <zhehuihu@usc.edu>
2023-06-03 10:53:32 -0700

    Modify: Search num_agents, num_workers x num_envs_per_worker * num_agents should be same for 8/16/32/64

---
+++ swarm_rl/runs/obstacles/quads_multi_obstacles_num_agents_32.py
@@ -0,0 +1,25 @@
+from sample_factory.launcher.run_description import Experiment, ParamGrid, RunDescription
+from swarm_rl.runs.obstacles.quad_obstacle_baseline import QUAD_BASELINE_CLI_8
+
+_params = ParamGrid(
+    [
+        ("seed", [0000, 1111, 2222, 3333]),
+        ("quads_num_agents", [32]),
+        ("num_workers", [18]),
+        ("num_envs_per_worker", [2]),
+    ]
+)
+
+OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
+    ' --quads_neighbor_visible_num=6 --quads_neighbor_obs_type=pos_vel --quads_encoder_type=attention '
+    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones '
+    '--wandb_group=final_search_num_agents'
+)
+
+_experiment = Experiment(
+    "final_search_num_agents_32",
+    OBSTACLE_MODEL_CLI,
+    _params.generate_params(randomize=False),
+)
+
+RUN_DESCRIPTION = RunDescription("obstacles_multi", experiments=[_experiment])
