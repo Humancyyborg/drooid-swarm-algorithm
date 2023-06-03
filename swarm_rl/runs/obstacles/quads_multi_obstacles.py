20f60734a49fc72b966c206b987a1b173b8bf9e8
Zhehui Huang <zhehuihu@usc.edu>
2023-06-03 01:42:36 -0700

    Search num_agents

--- swarm_rl/runs/obstacles/quads_multi_obstacles.py
+++ swarm_rl/runs/obstacles/quads_multi_obstacles.py
@@ -3,20 +3,20 @@ from swarm_rl.runs.obstacles.quad_obstacle_baseline import QUAD_BASELINE_CLI_8
 
 _params = ParamGrid(
     [
-        ("seed", [0000, 1111]),
-        ("max_policy_lag", [150, 100000000]),
+        ("seed", [0000, 1111, 2222, 3333]),
+        ("quads_num_agents", [16, 32, 64]),
     ]
 )
 
 OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
-    ' --num_workers=36 --num_envs_per_worker=4 --quads_num_agents=8 '
+    ' --num_workers=36 --num_envs_per_worker=4 '
     '--quads_neighbor_visible_num=6 --quads_neighbor_obs_type=pos_vel --quads_encoder_type=attention '
     '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones '
-    '--wandb_group=final_policy_lag'
+    '--wandb_group=final_search_num_agents'
 )
 
 _experiment = Experiment(
-    "final_policy_lag",
+    "final_search_num_agents",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
