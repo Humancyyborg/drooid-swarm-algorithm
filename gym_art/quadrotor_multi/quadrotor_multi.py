Bug fix: gradient explosion

gym_art/quadrotor_multi/quadrotor_multi.py
@@ -233,13 +233,13 @@ class QuadrotorEnvMulti(gym.Env):
     def extend_obs_space(self, obs, closest_drones):
         obs_neighbors = []
         if self.neighbor_obs_type == 'range':
-            obs_neighbors = -100 * np.ones((self.num_agents, self.neighbor_obs_size * (self.num_agents - 1)))
-            obs_neighbors[:, :self.neighbor_obs_size] = -10
+            obs_neighbors = -20.0 * np.ones((self.num_agents, self.neighbor_obs_size * (self.num_agents - 1)))
+            obs_neighbors[:, :self.neighbor_obs_size] = np.array([self.neighbor_range, 0., 0., 0., 0., 0.])
 
             for i in range(len(self.envs)):
                 if len(closest_drones[i]) > 0:
                     obs_neighbor_rel = self.get_obs_neighbor_rel(env_id=i, closest_drones=closest_drones).reshape(-1)
-                    obs_neighbor_rel = np.clip(obs_neighbor_rel, a_min=-9.9, a_max=9.9)
+                    obs_neighbor_rel = np.clip(obs_neighbor_rel, a_min=-6.0, a_max=6.0)
                     obs_neighbors[i, :len(obs_neighbor_rel)] = obs_neighbor_rel
         else:
             for i in range(len(self.envs)):

swarm_rl/models/quad_multi_model.py
@@ -76,9 +76,7 @@ class QuadNeighborhoodEncoderDeepsets(QuadNeighborhoodEncoder):
             tensor_obs_neighbor = obs_neighbors.reshape(-1, self.neighbor_obs_dim)
             # neighbor_embeds: (all groups, hidden_size)
             neighbor_embeds = self.embedding_mlp(tensor_obs_neighbor)
-            neighbor_embeds = neighbor_embeds.reshape(batch_size,
-                                                      int(torch.unique(neighbor_loc).item() / self.neighbor_obs_dim),
-                                                      self.neighbor_hidden_size)
+            neighbor_embeds = neighbor_embeds.reshape(batch_size, -1, self.neighbor_hidden_size)
             mean_embed = torch.mean(neighbor_embeds, dim=1)
 
         return mean_embed