8075bcb27065a4e0bcacc2b4cea87618fcb384a0
Zhehui Huang <zhehuihu@usc.edu>
2023-08-26 14:37:11 -0700

    Modify running scripts

--- swarm_rl/runs/hybrid/quads_hybrid_grid_sbc_mellinger.py
+++ swarm_rl/runs/hybrid/quads_hybrid_grid_sbc_mellinger.py
@@ -13,11 +13,11 @@ OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     ' --anneal_collision_steps=300000000 --quads_anneal_safe_start_steps=0 --quads_anneal_safe_total_steps=300000000 '
     '--quads_cost_rl_sbc=0.0 --quads_cost_pos=1.0 '
     '--with_wandb=True --wandb_project=Quad-Hybrid --wandb_user=multi-drones '
-    '--wandb_group=grid_search_sbc_mellinger --max_policy_lag=100000000'
+    '--wandb_group=grid_search_rl_mellinger --max_policy_lag=100000000'
 )
 
 _experiment = Experiment(
-    "grid_search_sbc_mellinger",
+    "grid_search_rl_mellinger",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
