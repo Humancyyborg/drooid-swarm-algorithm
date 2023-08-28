ac73b697eb7138dc1f4eb960d44e949d6e2b9359
Zhehui Huang <zhehuihu@usc.edu>
2023-08-28 16:31:11 -0700

    Minor modifications

--- swarm_rl/runs/hybrid/quads_hybrid_grid_sbc_specific.py
+++ swarm_rl/runs/hybrid/quads_hybrid_grid_sbc_specific.py
@@ -10,7 +10,8 @@ _params = ParamGrid(
 )
 
 OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
-    ' --anneal_collision_steps=300000000 --quads_cost_pos=1.0 --quads_cost_sbc_mellinger=0.0 '
+    ' --anneal_collision_steps=300000000 --quads_anneal_safe_start_steps=0 --quads_anneal_safe_total_steps=300000000 '
+    '--quads_cost_pos=1.0 --quads_cost_sbc_mellinger=0.0 '
     '--with_wandb=True --wandb_project=Quad-Hybrid --wandb_user=multi-drones --wandb_group=grid_search_sbc_radius'
 )
 
