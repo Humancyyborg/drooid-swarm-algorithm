8bf14e3f832cd67d0107a873758ad81863d120ac
Zhehui Huang <zhehuihu@usc.edu>
2023-09-08 13:20:32 -0700

    Add running script

--- swarm_rl/runs/obstacles/quads_multi_obstacles_density.py
+++ swarm_rl/runs/obstacles/quads_multi_obstacles_density.py
@@ -4,19 +4,19 @@ from swarm_rl.runs.obstacles.quad_obstacle_baseline import QUAD_BASELINE_CLI_8
 _params = ParamGrid(
     [
         ("seed", [0000, 1111, 2222, 3333]),
-        ("quads_obst_density", [0.4, 0.6]),
+        ("quads_obst_density", [0.8]),
     ]
 )
 
 OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     ' --num_workers=36 --num_envs_per_worker=4 '
-    '--quads_neighbor_visible_num=6 --quads_neighbor_obs_type=pos_vel --quads_encoder_type=attention '
+    '--quads_neighbor_visible_num=2 --quads_neighbor_obs_type=pos_vel --quads_encoder_type=attention '
     '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones '
-    '--wandb_group=final_search_density'
+    '--wandb_group=paper_search_density_8 --max_policy_lag=150'
 )
 
 _experiment = Experiment(
-    "final_search_density",
+    "paper_search_density_8",
     OBSTACLE_MODEL_CLI,
     _params.generate_params(randomize=False),
 )
---
+++ train_aws.sh
@@ -0,0 +1,1 @@
+python -m sample_factory.launcher.run --run=swarm_rl.runs.obstacles.quads_multi_obstacles_density --backend=processes --max_parallel=4 --pause_between=1 --experiments_per_gpu=4 --num_gpus=1
