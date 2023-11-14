d6c51b3d12f1b0d1973b955a984604e385bb087a
Zhehui Huang <zhehuihu@usc.edu>
2023-11-14 13:43:17 -0800

    search sbc mellinger

--- train_brain.sh
+++ train_brain.sh
@@ -1,5 +1,5 @@
 python -m sample_factory.launcher.run \
---run=swarm_rl.runs.hybrid.quads_hybrid_q_8_grid_sbc_aggressive_sparse \
+--run=swarm_rl.runs.hybrid.quads_hybrid_q_8_grid_sbc_aggressive_sparse_v1 \
 --backend=slurm --slurm_workdir=slurm_output \
 --experiment_suffix=slurm --pause_between=1 \
 --slurm_gpus_per_job=1 --slurm_cpus_per_gpu=16 \
