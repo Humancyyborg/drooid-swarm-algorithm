d585c080ebaed93fff7d86f4adc14ad114ec17f7
Zhehui Huang <zhehuihu@usc.edu>
2023-11-13 16:10:47 -0800

    Check sparse env

---
+++ swarm_rl/runs/hybrid/quads_hybrid_q_8_grid_sbc_aggressive_sparse.py
@@ -0,0 +1,28 @@
+from sample_factory.launcher.run_description import Experiment, ParamGrid, RunDescription
+from swarm_rl.runs.hybrid.baseline import QUAD_BASELINE_CLI_8
+
+_params = ParamGrid(
+    [
+        ("seed", [0000, 1111, 2222, 3333]),
+        ("quads_max_acc", [6.0]),
+        ("quads_max_neighbor_aggressive", [50.0]),
+        ("quads_max_obst_aggressive", [50.0]),
+    ]
+)
+
+OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
+    ' --anneal_collision_steps=300000000 --quads_anneal_safe_start_steps=0 --quads_anneal_safe_total_steps=300000000 '
+    '--quads_num_agents=8 --num_workers=36 --num_envs_per_worker=4 --quads_obst_density=0.2 --quads_obst_size=0.6 '
+    '--quads_cost_rl_sbc=0.1 --quads_cost_pos=1.0 --quads_cost_sbc_mellinger=0.0 --quads_neighbor_range=3.0 '
+    '--quads_obst_range=3.0 --quads_sbc_radius=0.05 --quads_obst_safe_coeff=1.0 --quads_sbc_boundary=0.1 '
+    '--with_wandb=True --wandb_project=Quad-Hybrid --wandb_user=multi-drones '
+    '--wandb_group=quad_8_sparse'
+)
+
+_experiment = Experiment(
+    "quad_8_sparse",
+    OBSTACLE_MODEL_CLI,
+    _params.generate_params(randomize=False),
+)
+
+RUN_DESCRIPTION = RunDescription("hybrid", experiments=[_experiment])
