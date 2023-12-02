fcda483b79e2ef0e51f112ccd026636e62b41642
Zhehui Huang <zhehuihu@usc.edu>
2023-12-01 22:39:21 -0800

    Modify for single drone

--- swarm_rl/runs/single_quad/single_quad.py
+++ swarm_rl/runs/single_quad/single_quad.py
@@ -6,7 +6,7 @@ _params = ParamGrid([
 ])
 
 SINGLE_CLI = QUAD_BASELINE_CLI + (
-    ' --with_wandb=False --wandb_project=Quad-Swarm-RL --wandb_group=single --wandb_user=multi-drones'
+    ' --with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_group=single_more_checkpoint --wandb_user=multi-drones'
 )
 
 _experiment = Experiment(
